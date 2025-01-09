f7a3f6529f0ac4f15971d639c501700b6a5eb324
jwolfson124 <65319207+jwolfson124@users.noreply.github.com>
2025-01-09 17:58:29 -0500

    Create Function_Library.py

---
+++ Function_Library.py
@@ -0,0 +1,8 @@
+Function: Assert
+Defintion: Will tell you if a line of code passes a certain logical test
+Example:
+x_float = 1
+assert x_float == 1
+assert x_float < 0
+The second will create an assertionerror because that specific line is not true
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
