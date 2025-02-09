2e2a4c9ef2eacc05067cb9aa65a770bb9aec5aeb
jwolfson124 <65319207+jwolfson124@users.noreply.github.com>
2025-02-09 13:29:22 -0500

    Update Libaries.py

--- Libaries.py
+++ Libaries.py
@@ -1,2 +1,5 @@
 Library line: from Decimal import Decimal
 use Decimal(num) to keep accuracy
+
+from pprint import pprint
+pprint(data) #this will print out the nested structures in an easier to read way
