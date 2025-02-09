d2e76a349b01cf6ecdf6040759b1e9bad3ec8f6d
jwolfson124 <65319207+jwolfson124@users.noreply.github.com>
2025-02-09 14:08:29 -0500

    Update Week 5: Sorting, Lambda, Slicing.py

--- Week 5: Sorting, Lambda, Slicing.py
+++ Week 5: Sorting, Lambda, Slicing.py
@@ -26,3 +26,46 @@ L[-3:] = [3,4,5]
 n = len(L)
 L[n-3:n]
 
+previous = ''
+
+for c in L:
+  if c != previous[-1:]: #this notation will not cause a failure when looping through
+    
+
+-------------------------------------------
+SAMPLE PROBLEM
+#take the length of the longest run and the letter associated
+test = {
+  'aaaabb' : (a, 4),
+  'bbbaaabaaaa' : (a,4),
+  'bbbaabbaaa' : b,3),
+  '' : ('', 0)
+}
+
+#solution 1
+#step 1: Initialize the outputs so that an empty string is going to give you the desired output
+def longest_consecutive_subsequence(s):
+  best, best_count = "",0
+  prev, prev_count = "", 0
+
+  #loop through all characters
+  for c in s:
+    if c == prev:
+      prev_count += 1
+
+    else:
+      #if the c does not match the previous character
+      if prev_count > best_count:
+        best = prev
+        best_count = prev_count
+        
+      prev, prevcount = c, 1
+    #check again at the end of the entire string to make sure that the last sequence is recorded
+  if prev_count > best_count:
+      best = prev
+      best_count = prev_count
+  return best, best_count
+#step 2:
+
+
+
